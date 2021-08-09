5e0ba42774bd8e120284e3d2613533d6e0e439b5
jae0107 <ghjgjh0107@gmail.com>
2021-08-10 06:01:56 +0930

    nav bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./topbar.scss";
+import { Person } from "@material-ui/icons";
 
 const Topbar = () => {
     return (
         <div className="topbar">
-            topbar
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                </div>
+                <div className="right">
+                <div className="itemContainer">
+                    <Person className="icon" />
+                    <span>0420603110</span>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
