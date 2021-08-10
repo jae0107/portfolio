6d6142437d055d4b723992162c4594336ddae318
jae0107 <ghjgjh0107@gmail.com>
2021-08-10 15:00:16 +0930

    side bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import "./topbar.scss";
-import { Person } from "@material-ui/icons";
+import { Mail, Person } from "@material-ui/icons";
 
-const Topbar = () => {
+const Topbar = ({ menuOpen, setMenuOpen }) => {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">genius.</a>
+                    <a href="#intro" className="logo">Jae Choi</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>0420603110</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>ghjgjh0107@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right">
-                <div className="itemContainer">
-                    <Person className="icon" />
-                    <span>0420603110</span>
-                </div>
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
