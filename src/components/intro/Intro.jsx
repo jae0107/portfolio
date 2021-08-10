8483c0b56ad4fa818c25762b8d4ec5259693ba61
jae0107 <ghjgjh0107@gmail.com>
2021-08-11 04:31:18 +0930

    intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./intro.scss";
+import { init } from "ityped";
 
 const Intro = () => {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+          showCursor: true,
+          backDelay: 1500,
+          backSpeed:60,
+          strings: ["Developer", "Software Engineer"],
+        });
+      }, []);
+
     return (
         <div className="intro" id="intro">
-            intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/IMG_2110-removebg-preview.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Jae Choi</h1>
+                    <h3>Graduate <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
