9513e1f702a55e91627db3accca889f5a6c449fb
jae0107 <ghjgjh0107@gmail.com>
2021-08-13 08:02:59 +0930

    emailing fuction

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
+import emailjs from 'emailjs-com';
 import "./contact.scss";
 
 const Contact = () => {
     const [message, setMessage] = useState(false);
 
-    const handleSubmit = (e) => {
+    const sendEmail = (e) => {
         e.preventDefault();
         setMessage(true);
-    };
+
+        emailjs.sendForm('service_xzjiopv', 'template_majg11p', e.target, 'user_RBiQ8bX7Svkz7GLWO4vLd')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset();
+    }
 
     return (
         <div className="contact" id="contact">
@@ -16,10 +26,11 @@ const Contact = () => {
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email" />
-                    <textarea placeholder="Message"></textarea>
-                    <button type="submit">Send</button>
+                <form className="contact-form" onSubmit={sendEmail}>
+                    <input type="text" name="from_name" placeholder="Your Name"/>
+                    <input type="email" name="from_email" placeholder="Your Email"/>
+                    <textarea name="message" placeholder="Message"/>
+                    <button type="submit" value="Send" >Send</button>
                     {message && <span>Thanks, I'll reply ASAP :) </span>}
                 </form>
             </div>
