06fe8bb52be79c700a3f78655269ed7497ce56bc
jae0107 <ghjgjh0107@gmail.com>
2021-08-13 14:27:41 +0930

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import emailjs from 'emailjs-com';
 import "./contact.scss";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
     const [message, setMessage] = useState(false);
@@ -22,14 +25,48 @@ const Contact = () => {
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <img src="assets/shake.svg" alt="" />
+                {/*<img src="assets/shake.svg" alt="" />*/}
+                <div className="contact-info">
+                    <div className="phone">
+                        <h2>Call</h2>
+                        <a href="tel:+61420603110">0420603110</a>
+                    </div>
+                    <div className="email">
+                        <h2>Email</h2>
+                        <a href="mailto:ghjgjh0107@gmail.com">ghjgjh0107@gmail.com</a>
+                    </div>
+                    <div className="itemContainer">
+                        <Link style={{ color: '#15023a' }} to={{ pathname: "https://www.linkedin.com/in/jae-choi-11422a185/" }} target="_blank" >
+                            <LinkedInIcon className="icon" />
+                        </Link>
+                        <Link style={{ color: '#15023a', marginLeft: '30px' }} to={{ pathname: "https://github.com/jae0107" }} target="_blank" >
+                            <GitHubIcon className="icon" />
+                        </Link>
+                    </div>
+                    <div className="location">
+                        <h2>Location</h2>
+                        Gosse International Student Residence<br></br>
+                        Room 26, 2 Highgate St<br></br>
+                        Fullarton SA 5063<br></br>
+                        <a className="btn btn-accent" href="https://www.google.com/maps/place/Gosse+International+Student+Residence/@-34.9572511,138.620861,15z/data=!4m5!3m4!1s0x0:0xa70352ed2ab3e671!8m2!3d-34.9572511!4d138.620861" >Google Map</a>
+                    </div>
+                </div>
             </div>
             <div className="right">
                 <h2>Contact</h2>
                 <form className="contact-form" onSubmit={sendEmail}>
-                    <input type="text" name="from_name" placeholder="Your Name"/>
-                    <input type="email" name="from_email" placeholder="Your Email"/>
-                    <textarea id="textarea" name="message" placeholder="Message"/>
+                    <div class="form-group">
+                        <label className="sr-only" for="fname">Name *</label>
+                        <input className="required form-control" id="fname" placeholder="Name&nbsp;*" type="text" name="from_name" />
+                    </div>
+                    <div className="form-group">
+                        <label className="sr-only" for="contactEmail">Email *</label>
+                        <input type="email" name="from_email" className="required form-control h5-email" id="contactEmail" placeholder="Email&nbsp;*"></input>
+                    </div>
+                    <div className="form-group">
+                        <label className="sr-only" for="comment">Type your message here</label>
+                        <textarea className="required form-control" id="textarea" name="message" placeholder="Type your message here&nbsp;*" rows="6"></textarea>
+                    </div>
                     <button type="submit" value="Send" >Send</button>
                     {message && <span>Thanks, I'll reply ASAP :) </span>}
                     <div id="copy"></div>
