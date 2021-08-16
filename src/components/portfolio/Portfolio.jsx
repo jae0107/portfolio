b280c84500dc3d4858eaaab40d976af5487d78da
jae0107 <ghjgjh0107@gmail.com>
2021-08-16 13:28:46 +0930

    portfolio list

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,64 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import PortfolioList from '../portfolioList/PortfolioList';
 import "./portfolio.scss";
-import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio, } from "../../data";
+import { featuredPortfolio } from "../../data";
 
 const Portfolio = () => {
-    const [selected, setSelected] = useState("featured");
     const [data, setData] = useState([]);
-    const list = [
-        {
-          id: "featured",
-          title: "Featured",
-        },
-        {
-          id: "web",
-          title: "Web App",
-        },
-        {
-          id: "mobile",
-          title: "Mobile App",
-        },
-        {
-          id: "design",
-          title: "Design",
-        },
-        {
-          id: "content",
-          title: "Content",
-        },
-    ];
-
     useEffect(() => {
-        switch (selected) {
-            case "featured":
-                setData(featuredPortfolio);
-                break;
-            case "web":
-                setData(webPortfolio);
-                break;
-            case "mobile":
-                setData(mobilePortfolio);
-                break;
-            case "design":
-                setData(designPortfolio);
-                break;
-            case "content":
-                setData(contentPortfolio);
-                break;
-            default:
-                setData(featuredPortfolio);
-        }
-    }, [selected]);
+        setData(featuredPortfolio);
+    }, []);
 
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
-            <ul>
-                {list.map(item => (
-                    <PortfolioList key={item.id} title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
-                ))}
-            </ul>
             <div className="container">
                 {data.map(datum => (
                     <div key={datum.id} className="item">
