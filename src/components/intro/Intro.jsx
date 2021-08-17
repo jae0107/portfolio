0224daea83b46b0d9253113bc12401be8e9a9c1e
jae0107 <ghjgjh0107@gmail.com>
2021-08-17 18:29:00 +0930

    change img

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ const Intro = () => {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/IMG_2110-removebg-preview.png" alt="" />
+                    <img src="assets/man.png" alt="" />
                 </div>
             </div>
             <div className="right">
