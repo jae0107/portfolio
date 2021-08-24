dfc98020fb935c71d442a33b4455955f045224f3
jae0107 <ghjgjh0107@gmail.com>
2021-08-25 04:17:34 +0930

    mobile view

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,7 +5,7 @@ const Menu = ({ menuOpen, setMenuOpen }) => {
     return (
         <div className={"menu "+(menuOpen && "active")}>
             <ul>
-                <li onClick={()=>setMenuOpen(false)}>
+                <li className="first" onClick={()=>setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
