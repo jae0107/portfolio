bc57879fa0ffa344fff278a3d431a35a51699890
jae0107 <ghjgjh0107@gmail.com>
2021-10-25 11:00:37 +1030

    viewport test

--- cypress/integration/myTest/homepageTest.js
+++ cypress/integration/myTest/homepageTest.js
@@ -6,9 +6,31 @@ describe("renders the home page", () => {
     });
   
     it("renders correctly", () => {
+      cy.viewport('macbook-16');
+
       cy.get("#container").should("exist");
+      cy.get("#intro").should("exist");
+      cy.get("#topbar").should("exist");
+      cy.get("#menu").should("exist");
+      cy.get("#skills").should("exist");
+      cy.get("#portfolio").should("exist");
+      cy.get("#projects").should("exist");
+      cy.get("#contact").should("exist");
     });
 
+    it("mobile testing", () => {
+      cy.viewport('iphone-8');
+
+      cy.get("#container").should("exist");
+      cy.get("#intro").should("exist");
+      cy.get("#topbar").should("exist");
+      cy.get("#menu").should("exist");
+      cy.get("#skills").should("exist");
+      cy.get("#portfolio").should("exist");
+      cy.get("#projects").should("exist");
+      cy.get("#contact").should("exist");
+    });
+/*
     it("intro component test", () => {
       cy.get("#intro").should("exist");
     });
@@ -35,5 +57,5 @@ describe("renders the home page", () => {
 
     it("contact component test", () => {
       cy.get("#contact").should("exist");
-    });
+    });*/
 });
