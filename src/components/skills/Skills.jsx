3c720ae1f200d6288f8ece186077177c5e32b9a4
jae0107 <ghjgjh0107@gmail.com>
2021-11-04 03:11:24 +1030

    update skiils

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,7 +7,28 @@ const Skills = () => {
             <h1>Skills</h1>
             <div className="container">
                 <div className="imgContainer">
+                    <img src="assets/java-logo.png" alt="java" />
+                </div>
+                <div className="imgContainer">
+                    <img src="assets/c-plus-plus-logo.png" alt="C++" />
+                </div>
+                <div className="imgContainer">
+                    <img src="assets/javascript-icon.png" alt="JavaScript" />
+                </div>
+                <div className="imgContainer">
                     <img src="assets/react-icon.png" alt="React" />
+                </div>
+                <div className="imgContainer">
+                    <img src="assets/angular-icon.svg" alt="Angular" />
+                </div>
+                <div className="imgContainer">
+                    <img src="assets/p5js-icon.svg" alt="p5.js" />
+                </div>
+                <div className="imgContainer">
+                    <img src="assets/html5-icon.png" alt="HTML5" />
+                </div>
+                <div className="imgContainer">
+                    <img src="assets/google-apps-script-icon.png" alt="Google_app_script" />
                 </div>
                 <div className="imgContainer">
                     <img src="assets/express-icon.png" alt="Express" />
@@ -16,34 +37,13 @@ const Skills = () => {
                     <img src="assets/node-icon.png" alt="Node" />
                 </div>
                 <div className="imgContainer">
-                    <img src="assets/angular-icon.svg" alt="Angular" />
-                </div>
-                <div className="imgContainer">
                     <img src="assets/ruby_on_rails-icon.png" alt="Ruby_on_Rails" />
-                </div>
-                <div className="imgContainer">
-                    <img src="assets/p5js-icon.svg" alt="p5.js" />
-                </div>
-                <div className="imgContainer">
-                    <img src="assets/google-apps-script-icon.png" alt="Google_app_script" />
                 </div>
                 <div className="imgContainer">
                     <img src="assets/firebase-icon.png" alt="firebase" />
                 </div>
                 <div className="imgContainer">
                     <img src="assets/heroku-logo.png" alt="heroku" />
-                </div>
-                <div className="imgContainer">
-                    <img src="assets/java-logo.png" alt="java" />
-                </div>
-                <div className="imgContainer">
-                    <img src="assets/javascript-icon.png" alt="JavaScript" />
-                </div>
-                <div className="imgContainer">
-                    <img src="assets/html5-icon.png" alt="HTML5" />
-                </div>
-                <div className="imgContainer">
-                    <img src="assets/c-plus-plus-logo.png" alt="C++" />
                 </div>
                 <div className="imgContainer">
                     <img src="assets/git-icon.png" alt="git" />
